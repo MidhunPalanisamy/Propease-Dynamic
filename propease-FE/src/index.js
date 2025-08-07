143680b93cb298bcafecc03130790e1cd68183af
Midhun Palanisamy <midhunpalanisamy@Midhun-2.local>
2025-08-07 11:42:42 +0530

    Fix routing and GitHub Pages deployment

--- propease-FE/src/index.js
+++ propease-FE/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'leaflet/dist/leaflet.css';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
